6e2ab751e9e6304e7cc5565c472ae5804c4b03e6
AmanGitkumar <kumaraman55475@gmail.com>
2024-12-31 18:42:57 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menLink = document.getElementById('menLink');
+    menLink.addEventListener('click', () => {
+        alert('Ha Ha Ha.....');
+    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const womenLink = document.getElementById('womenLink');
+    womenLink.addEventListener('click', () => {
+        alert('Ha Ha Ha.....');
+    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const beautyLink = document.getElementById('beautyLink');
+    beautyLink.addEventListener('click', () => {
+        alert('Ha Ha Ha.....');
+    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const homeLink = document.getElementById('homeLink');
+    homeLink.addEventListener('click', () => {
+        alert('Ha Ha Ha.....');
+    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const bagLink = document.getElementById('bagLink');
+    bagLink.addEventListener('click', () => {
+        alert('Ha Ha Ha.....');
+    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const proLink = document.getElementById('proLink');
+    proLink.addEventListener('click', () => {
+        alert('Ha Ha Ha.....');
+    });
+});
+document.addEventListener('DOMContentLoaded', () => {
+    const wishLink = document.getElementById('wishLink');
+    wishLink.addEventListener('click', () => {
+        alert('Ha Ha Ha.....');
+    });
+});
